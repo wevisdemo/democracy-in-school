9595f7e4bc2862133e8a389dd47b8d32b56a6cfc
Petch Songpon <petch.s@finnomena.com>
2023-01-22 18:23:36 +0700

    change home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Ending from 'components/ending/ending'
+import Navbar from 'components/navbar'
+import Quiz from 'components/quiz/quiz'
+import Decision from 'components/templates/decision'
+import Intro from 'components/templates/intro'
+import Welcome from 'components/templates/welcome'
+import styled from 'styled-components'
+
+const MainComponent = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  background: url('background/bg_white.01.png');
+  overflow: hidden;
+`
 
 export default function Home() {
-  return <></>
+  return (
+    <MainComponent>
+      <Navbar></Navbar>
+      <Welcome></Welcome>
+      <Intro></Intro>
+      <Decision></Decision>
+      <Quiz></Quiz>
+      <Ending></Ending>
+    </MainComponent>
+  )
 }
